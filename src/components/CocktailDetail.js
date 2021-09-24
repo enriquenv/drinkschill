08c55a13dd9054a7efce0a753b529cc740eb418f
Enrique NV <enriquen@live.de>
2021-09-24 15:29:12 +0200

    check3

--- src/components/CocktailDetail.js
+++ src/components/CocktailDetail.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 import './CocktailDetail.css'
 import { Link } from 'react-router-dom'
 
 class CocktailDetail extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      details: []
+      details: [],
     }
   }
   componentDidMount() {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.props.match.params.id}`)
+    fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.props.match.params.id}`
+    )
       .then(res => res.json())
-      .then(data => this.setState({ details: data.drinks[0] }));
+      .then(data => this.setState({ details: data.drinks[0] }))
   }
   render() {
     console.log('details:', this.state.details)
     return (
       <div className="details">
-          <figure>
-            <img src={this.state.details.strDrinkThumb} />
-          </figure>
-          <article>
-            <h2>
-              {this.state.details.strDrink}
-            </h2>
-            <h3>Zutaten</h3>
-            {/* durch ingredients mappen */}
-            <p>
-              {this.state.details.strInstructions}
-            </p>
-          </article>
+        <figure>
+          <img src={this.state.details.strDrinkThumb} />
+        </figure>
+        <article>
+          <h2>{this.state.details.strDrink}</h2>
+          <h3>Zutaten</h3>
+          {/* durch ingredients mappen */}
+          <p>{this.state.details.strInstructions}</p>
+        </article>
       </div>
-    );
+    )
   }
 }
 
-export default CocktailDetail;
+export default CocktailDetail
