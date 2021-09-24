a0b0a4cc67fdaa9c9946070850d16b63574d9c11
Enrique NV <enriquen@live.de>
2021-09-24 16:18:42 +0200

    check5

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 class SearchResults extends React.Component {
-<<<<<<< HEAD
   componentDidMount() {
     fetch(
       `https://thecocktaildb.com/api/json/v1/1/search.php?s=${this.props.match.params.search}`
@@ -21,24 +20,3 @@ class SearchResults extends React.Component {
 }
 
 export default SearchResults
-=======
-    componentDidMount() {
-        fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${this.props.match.params.search}`).then(async response => {
-            try {
-                const data = await response.json()
-                console.log('response data?', data)
-            } catch (error) {
-                console.log('Error happened here!')
-                console.error(error)
-            }
-        })
-    }
-    render() {
-        return <div>
-            test
-        </div>;
-    }
-}
-
-export default SearchResults;
->>>>>>> 80cebf2a300c11eb98846a3e2edc817fa9bc2656
