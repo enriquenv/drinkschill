b626fdee792cf1edc3dce97a96a3cfd3f16c13da
enriquenv <enriquen@live.de>
2021-09-24 17:32:16 +0200

    CocktailDetails

--- src/components/CocktailDetail.js
+++ src/components/CocktailDetail.js
@@ -19,17 +19,32 @@ class CocktailDetail extends Component {
     console.log('details:', this.state.details)
     return (
       <div className="details">
-          <figure className="leftimage">
-            <img src={this.state.details.strDrinkThumb} />
+          <figure>
+            <img className="detail_image" src={this.state.details.strDrinkThumb} />
           </figure>
-          <article className="rightarticle">
+          <article className="right_article">
             <h2>
               {this.state.details.strDrink}
             </h2>
             <h3>Zutaten</h3>
             {/* durch ingredients mappen */}
+            <span className="detail_ingredient_item">{this.state.details.strIngredient1 ? this.state.details.strIngredient1 : console.log("keine this.state.details.strIngredient1")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient2 ? this.state.details.strIngredient2 : console.log("keine this.state.details.strIngredient2")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient3 ? this.state.details.strIngredient3 : console.log("keine this.state.details.strIngredient3")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient4 ? this.state.details.strIngredient4 : console.log("keine this.state.details.strIngredient4")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient5 ? this.state.details.strIngredient5 : console.log("keine this.state.details.strIngredient5")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient6 ? this.state.details.strIngredient6 : console.log("keine this.state.details.strIngredient6")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient7 ? this.state.details.strIngredient7 : console.log("keine this.state.details.strIngredient7")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient8 ? this.state.details.strIngredient8 : console.log("keine this.state.details.strIngredient8")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient9 ? this.state.details.strIngredient9 : console.log("keine this.state.details.strIngredient9")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient10 ? this.state.details.strIngredient10 : console.log("keine this.state.details.strIngredient10")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient11 ? this.state.details.strIngredient11 : console.log("keine this.state.details.strIngredient11")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient12 ? this.state.details.strIngredient12 : console.log("keine this.state.details.strIngredient12")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient13 ? this.state.details.strIngredient13 : console.log("keine this.state.details.strIngredient13")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient14 ? this.state.details.strIngredient14 : console.log("keine this.state.details.strIngredient14")}</span>
+            <span className="detail_ingredient_item">{this.state.details.strIngredient15 ? this.state.details.strIngredient15 : console.log("keine this.state.details.strIngredient15")}</span>
             <p>
-              {this.state.details.strInstructions}
+            <span className="detail_instructions">{this.state.details.strInstructions}</span>
             </p>
           </article>
       </div>
