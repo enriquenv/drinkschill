c2e947a8667795e2766dc69ecbe986b950b071d2
Enrique NV <enriquen@live.de>
2021-09-27 09:23:05 +0200

    pull

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-import './Drink.css';
+import { Link } from 'react-router-dom'
+import React from 'react'
+import './Drink.css'
 
-const Drink = (props) => {
-    return (
-        <Link to={`/cocktail-detail/${props.id}`}>
-            <div className={'drink-container'}>
-                <img src={props.img} alt={props.name} className={'drink-image'} />
-                <h2 className={'drink-name'}>{props.name}</h2>
-            </div>
-        </Link>
-    );
+const Drink = props => {
+  return (
+    <main className={'cocktails'}>
+      <Link to={`/cocktail-detail/${props.id}`}>
+        <div className={'drink-container'}>
+          <img src={props.img} alt={props.name} className={'drink-image'} />
+          <h2 className={'drink-name'}>{props.name}</h2>
+        </div>
+      </Link>
+    </main>
+  )
 }
 
-export default Drink;
+export default Drink
