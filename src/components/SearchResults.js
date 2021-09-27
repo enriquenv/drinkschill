b2f807c6e1eb1704fc38867b5870ee29ff3c12c3
Enrique NV <enriquen@live.de>
2021-09-27 11:08:00 +0200

    search function

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import Drink from "./Drink";
 
 class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drinks: []
+    }
+  }
   componentDidMount() {
-    fetch(
-      `https://thecocktaildb.com/api/json/v1/1/search.php?s=${this.props.match.params.search}`
-    ).then(async response => {
-      try {
-        const data = await response.json()
-        console.log('response data?', data)
-      } catch (error) {
-        console.log('Error happened here!')
-        console.error(error)
-      }
-    })
+    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${this.props.match.params.search}`)
+      .then(res => res.json())
+      .then(data => this.setState({ drinks: data.drinks }))
   }
   render() {
-    return <div>test</div>
+    return (
+      <div className={'category-results-container'}>
+        {this.state.drinks.map((e, i) => <div className={`style${Math.floor((i % 6) + 1)}`}><Drink key={e.idDrink} id={e.idDrink} name={e.strDrink} img={e.strDrinkThumb} /></div>)}
+      </div>
+    )
   }
 }
 
